03ae040cfe9f8e26babc10bf2b10c6334b6da1fb
Youssef-Msadaa <yousseffmsadaa@gmail.com>
2026-05-21 15:47:49 +0200

    handle status for redundant checkpoints

--- src/main/java/com/example/dto/CheckPoint.java
+++ src/main/java/com/example/dto/CheckPoint.java
@@ -9,14 +9,19 @@ public class CheckPoint implements Serializable {
     private final UUID employeeId;
     private final LocalDateTime time;
     private final boolean isCheckIn;
+    private String statut;
 
-    public CheckPoint(UUID employeeId, LocalDateTime time,boolean isCheckIn) {
+
+    public CheckPoint(UUID employeeId, LocalDateTime time, boolean isCheckIn) {
         this.employeeId = employeeId;
         this.time = time;
         this.isCheckIn = isCheckIn;
+        this.statut = "normal";
     }
 
     public UUID getEmployeeId() { return employeeId; }
     public LocalDateTime getTime() { return time; }
     public boolean isCheckIn() { return isCheckIn; }
+    public String getStatut() { return statut; }
+    public void setStatut(String statut) { this.statut = statut; }
 }
